ada77ea289160cbb17c9cbdcfac381a90a68107b
Yongjun <wangyj641@gmail.com>
2024-07-05 15:39:06 +0800

    update

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,5 +1,5 @@
 import Router from "@koa/router";
-import multer from "@koa/multer"
+//import multer from "@koa/multer"
 import { storeData } from "../db/data.js";
 import { readCSVandInsertData } from './util.js';
 
@@ -31,31 +31,31 @@ SensorRouter.post('/data', async ctx => {
   }
 });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './uploads');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   }
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
-SensorRouter.post('/csv', upload.single('file'), async ctx => {
-  console.log('POST a CSV file');
-  const file = ctx.request?.files?.fileInput;
+// SensorRouter.post('/csv', upload.single('file'), async ctx => {
+//   console.log('POST a CSV file');
+//   const file = ctx.request?.files?.fileInput;
 
-  if (!file) {
-    ctx.status = 400;
-    ctx.body = 'No file uploaded.';
-    return;
-  }
-  ctx.body = `File ${file.originalname} uploaded successfully.`;
+//   if (!file) {
+//     ctx.status = 400;
+//     ctx.body = 'No file uploaded.';
+//     return;
+//   }
+//   ctx.body = `File ${file.originalname} uploaded successfully.`;
 
-  const csv = file.filepath;
-  console.log('Download the CSV to: ' + csv);
-  readCSVandInsertData(csv);
-})
+//   const csv = file.filepath;
+//   console.log('Download the CSV to: ' + csv);
+//   readCSVandInsertData(csv);
+// })
 
 export default SensorRouter;
